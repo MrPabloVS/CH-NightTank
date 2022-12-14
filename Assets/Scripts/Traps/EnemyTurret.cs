ee6cbca46199c0efe1b88adc78ec483d2b81ec46
MrPabloVS <pablovazquezsoldano@gmail.com>
2022-12-14 16:07:47 -0300

    Add EnemyTurret Prefab

---
+++ Assets/Scripts/Traps/EnemyTurret.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyTurret : MonoBehaviour
+{
+    public float cooldown = 0f;
+    public Transform salida;
+    public GameObject Bullet;
+
+     private void Update()
+    {
+        cooldown -= Time.deltaTime;
+
+        if (cooldown < 0f)
+        {
+            Shoot();
+        }
+       
+		
+    }
+	
+	private void Shoot()
+	{
+		Debug.Log("The Cannon has been shoot!");
+
+		Instantiate(Bullet,salida.position,transform.rotation);
+        cooldown = 5f;
+
+	}
+
+}
